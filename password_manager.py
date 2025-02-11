59b12e13cd1a49968081712c1c0adb3606e93326
Logan <lmaurer3031@gmail.com>
2025-02-10 21:53:06 -0500

    Removed duplicate function after moving it to utils.py

--- password_manager.py
+++ password_manager.py
@@ -31,11 +31,4 @@ while True:
     else:
         break
 
-#I am taking the password length that the user enters and then printing randomly what the password is based on what characters they want to use.
-def password_generator():
-    length = int(input("Enter password length: "))
-    for i in range(length):
-        print(random.choice(final_char_pool), end="")
-    print()
-
 password_generator()
