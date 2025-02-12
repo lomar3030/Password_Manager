f40c65011e7409bf4c8a77252bc3f27c2dfce545
Logan <lmaurer3031@gmail.com>
2025-02-11 23:25:42 -0500

    Finished the menu

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,23 @@
 from utils import password_generator
 from storage import save_a_password
+from storage import retrieve_password
 
 #Menu
-print("Welcome to Password Manager!")
-print("1. Generate a new password.")
-print("2. Save a password.")
-print("3. Retrieve a password.")
-print("4. Exit")
-menu_choice = int(input("Please choose the number for the option that you want: "))
-if menu_choice == 1:
-    password_generator()
-elif menu_choice == 2:
-    save_a_password()
-
+while True:
+    print("Welcome to Password Manager!")
+    print("1. Generate a new password.")
+    print("2. Save a password.")
+    print("3. Retrieve a password.")
+    print("4. Exit")
+    menu_choice = int(input("Please choose the number for the option that you want: "))
+    if menu_choice == 1:
+        password_generator()
+    elif menu_choice == 2:
+        save_a_password()
+    elif menu_choice == 3:
+        retrieve_password()
+    elif menu_choice == 4:
+        print("Exiting now...\n")
+        break
+    else:
+        print("Enter a valid option") 
--- storage.py
+++ storage.py
@@ -1,7 +1,8 @@
 #This fuction will strore the password in the password in the passwords.json file.
 
+import json
+
 def save_a_password():
-    import json
     with open("passwords.json", "r") as file:
         data = json.load(file)
         url = input("Website name(URL): ")
@@ -15,3 +16,15 @@ def save_a_password():
 
         with open("passwords.json", "w") as file:
             json.dump(data, file, indent=4)
+
+
+def retrieve_password():
+    with open("passwords.json", "r") as file:
+        data = json.load(file)
+        url = input("Enter the URL of the password you are trying to find: ")
+        if url in data:
+            print(f"Username: {data[url]['Username']}")
+            print(f"Password: {data[url]['Password']}")
+        else:
+            print("No password saved for this site")
+
